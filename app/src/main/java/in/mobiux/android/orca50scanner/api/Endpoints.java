746173022dbd4e9bdfbaf21ea2a98d08a13b48e0
Sujeet Kumar <sujeet@mobiux.in>
2021-07-12 00:25:45 +0530

    added baseUrl for staging, internal ip & external ip

--- app/src/main/java/in/mobiux/android/orca50scanner/api/Endpoints.java
+++ app/src/main/java/in/mobiux/android/orca50scanner/api/Endpoints.java
@@ -2,6 +2,15 @@ package in.mobiux.android.orca50scanner.api;
 
 public class Endpoints {
 
+    //    SOInventory_SGUL_Staging_v01.apk
+    public static final String BASE_URL_STAGING = "https://footprints-staging.hito.solutions/api/v1/";
+
+    //    SOInventory_SGUL_Release_extIP_v01.apk
+    public static final String BASE_URL_GLOBAL = "https://stgeorge.hitoitsolutions.com/api/v1/";
+
+    //    SOInventory_SGUL_Release_intIP_v01.apk
+    public static final String BASE_URL_LOCAL = "https://172.21.78.80/api/v1/";
+
 
     public static final String BASE_URL = "https://footprints-staging.hito.solutions/api/v1/";
 
