d8e070da89c5104426b686e84e631ca51dd02075
Sujeet Kumar <sujeet@mobiux.in>
2021-10-22 19:25:31 +0530

    all apps is done

--- writetorfidtags/src/main/java/in/mobiux/android/orca50scanner/writetorfidtags/activity/MainActivity.java
+++ writetorfidtags/src/main/java/in/mobiux/android/orca50scanner/writetorfidtags/activity/MainActivity.java
@@ -66,7 +66,7 @@ public class MainActivity extends BaseActivity {
         ivSettings = findViewById(R.id.ivSettings);
         spnrTags = findViewById(R.id.spnrTags);
         btnAssign.setVisibility(View.GONE);
-        
+
         ivSettings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
